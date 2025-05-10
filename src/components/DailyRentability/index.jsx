814b59288776d20e65605c64d9daf2b04ecb3cdd
Thomasdevm <Thomasmadariaga2021@outlook.es>
2025-05-10 11:27:28 -0300

    feat: add DailyRentability component and styles, include Bootstrap dependency

---
+++ src/components/DailyRentability/index.jsx
@@ -0,0 +1,10 @@
+const DailyRentability = () =>{
+    return (
+        <div className="daily-rentability">
+            <h2>Daily</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia odio praesentium sequi dignissimos sint officiis hic aliquid aperiam nobis. Harum magni assumenda consectetur alias consequatur similique earum cupiditate recusandae et.</p>
+        </div>
+    )
+}
+
+export default DailyRentability
