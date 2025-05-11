3cc1dc66df7404d483e1daf4323afb97bda9d571
Thomasdevm <Thomasmadariaga2021@outlook.es>
2025-05-11 09:47:55 -0300

    feat: add Redux Toolkit and Recharts for data visualization
    - Updated package.json and package-lock.json to include @reduxjs/toolkit and recharts dependencies.
    - Refactored DailyRentability component to display an AreaChart using recharts.
    - Refactored RentSharesCrypto component to display a BarChart using recharts.
    - Refactored Rentability_long_shorts component to display a LineChart using recharts.
    - Refactored RoiRent component to display a LineChart using recharts.
    - Removed unnecessary border styles from CSS files for better aesthetics.

--- src/components/RoiRent/index.jsx
+++ src/components/RoiRent/index.jsx
@@ -1,10 +1,59 @@
+import { ResponsiveContainer, LineChart, Line } from "recharts";
 import "./RoiRent.css";
 const RoiRent = () => {
-return (
+    const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+  return (
     <div className="roi">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, pariatur saepe accusamus at magnam deserunt dicta nam temporibus eveniet. Qui minima aperiam expedita iusto eum officia ad illum id facere!</p>
+      <ResponsiveContainer>
+        <LineChart width={300} height={100} data={data}>
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
-)
-}
+  );
+};
 
 export default RoiRent;
