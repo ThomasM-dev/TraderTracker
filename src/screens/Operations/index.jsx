dd383e9a3865e62db9d2f240a0b81484cfdb49d9
Thomasdevm <Thomasmadariaga2021@outlook.es>
2025-05-12 06:33:41 -0300

    style: format code for better readability in Operations screen

--- src/screens/Operations/index.jsx
+++ src/screens/Operations/index.jsx
@@ -2,7 +2,7 @@ import { Row, Table, Container } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 const Operations = () => {
-    const operations = useSelector((state) => state.operations);    
+  const operations = useSelector((state) => state.operations);
   return (
     <Container className="mt-3">
       <Row>
@@ -16,19 +16,33 @@ const Operations = () => {
               <th className="bg-black text-white border-none">Fecha</th>
               <th className="bg-black text-white border-none">Tipo</th>
               <th className="bg-black text-white border-none">Cantidad</th>
-              <th className="bg-black text-white border-none">Ganancia/Pérdida</th>
+              <th className="bg-black text-white border-none">
+                Ganancia/Pérdida
+              </th>
               <th className="bg-black text-white border-none">ROI (%)</th>
             </tr>
           </thead>
           <tbody className="style-table border-0">
             {operations.map((operation) => (
               <tr key={operation.id} className="border-0">
-                <td className="bg-black text-white border-0">{operation.instrumento}</td>
-                <td className="bg-black text-white border-0">{operation.fecha}</td>
-                <td className="bg-black text-white border-0">{operation.tipo || operation.posición || operation.posicion}</td>
-                <td className="bg-black text-white border-0">{operation.cantidad || operation.tamaño}</td>
-                <td className="bg-black text-white border-0">{operation.variacion || operation.variación}%</td>
-                <td className="bg-black text-white border-0">{operation.ganancia}</td>
+                <td className="bg-black text-white border-0">
+                  {operation.instrumento}
+                </td>
+                <td className="bg-black text-white border-0">
+                  {operation.fecha}
+                </td>
+                <td className="bg-black text-white border-0">
+                  {operation.tipo || operation.posición || operation.posicion}
+                </td>
+                <td className="bg-black text-white border-0">
+                  {operation.cantidad || operation.tamaño}
+                </td>
+                <td className="bg-black text-white border-0">
+                  {operation.ganancia}
+                </td>
+                <td className="bg-black text-white border-0">
+                  {operation.variacion || operation.variación}%
+                </td>
               </tr>
             ))}
           </tbody>
