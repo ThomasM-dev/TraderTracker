0f75ca499e7a82307a7dcc2ab106752b5d132ee3
Thomasdevm <Thomasmadariaga2021@outlook.es>
2025-05-14 06:07:26 -0300

    fix: update asset references in hosting cache and refactor DailyRentability component to use BarChart

--- src/components/DailyRentability/index.jsx
+++ src/components/DailyRentability/index.jsx
@@ -1,44 +1,94 @@
+import { useSelector } from "react-redux";
 import "./DailyRentability.css";
 import {
-  AreaChart,
-  Area,
+  BarChart,
+  Bar,
+  Cell,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
+  Legend,
+  ReferenceLine,
   ResponsiveContainer,
 } from "recharts";
 
 const data = [
-  { name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
-  { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
-  { name: "Page C", uv: 2000, pv: 9800, amt: 2290 },
-  { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
-  { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
-  { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
-  { name: "Page G", uv: 3490, pv: 4300, amt: 2100 },
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: -3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: -2000,
+    pv: -9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: -1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: -3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
 ];
 
 const DailyRentability = () => {
+  const Operations = useSelector((state) => state.operations);
 
-  
   return (
-    <div className="daily-rentability mt-3">
-      <ResponsiveContainer width="100%" height={300}>
-        <AreaChart
+    <div className="daily-rentability mt-3 mb-5">
+      <h2 className="text-center">Rentabilidad Diaria</h2>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={300}
           data={data}
-          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" tick={{ fill: "#fff", fontSize: 12 }} />
-          <YAxis tick={{ fill: "#fff", fontSize: 12 }} />
+          <XAxis dataKey="name" />
+          <YAxis />
           <Tooltip
-            contentStyle={{ backgroundColor: "#000", color: "#0149a1" }}
+            contentStyle={{ backgroundColor: "#000", color: "#fff" }}
             labelStyle={{ color: "#fff" }}
             itemStyle={{ color: "#fff" }}
           />
-          <Area type="monotone" dataKey="uv" stroke="#0149a1" fill="#0149a1" />
-        </AreaChart>
+          <Legend />
+          <ReferenceLine y={0} stroke="#000" />
+          <Bar dataKey="pv" fill="#838383" />
+          <Bar dataKey="uv" fill="white" />
+        </BarChart>
       </ResponsiveContainer>
     </div>
   );
